4bda654cf585e1d5b442d5393bbdbef072f8b4e6
Bones <pentagonzero7@gmail.com>
2025-11-30 13:34:09 +0300

    Added hotbar (non functional)

--- UI_BoilerPlate/main.py
+++ UI_BoilerPlate/main.py
@@ -1,12 +1,18 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
 
-class NavigationManger():
-    pass    
+Builder.load_file("main.kv")
+
+class NavigationManager(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 class MainApp(App):
     def build(self):
-        return NavigationManger()
+        return NavigationManager()
 
 
 if __name__ == "__main__":
