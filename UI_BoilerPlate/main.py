9e9517768fd5c6b27c209ff9a5bf86439a6c1fc2
Bones <pentagonzero7@gmail.com>
2025-12-01 14:23:25 +0300

    Working buttons

--- UI_BoilerPlate/main.py
+++ UI_BoilerPlate/main.py
@@ -1,3 +1,4 @@
+from kivy.uix.filechooser import ScreenManager
 from kivy.uix.accordion import NumericProperty
 from kivy.app import App
 from kivy.uix.label import Label
@@ -5,14 +6,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 import kivy.utils as utils
-from kivy.properties import NumericProperty()
 
 Builder.load_file("main.kv")
 
 class NavigationManager(Widget):
-    def screen_switcher(self, instance, value)
-        counter = self.ids.screen_manager.current_screen
-        print("Screen changed to:", value)
+    def screen_switcher(self, instance):
+        ScreenManager = self.ids.current_screen
+        ScreenManager.current = instance
+        print("Screen changed to:", instance)
+
 class MainApp(App):
     def build(self):
         return NavigationManager()
