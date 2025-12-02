0e933195d82756963090fb4e897282a4281a8b5f
Bones <pentagonzero7@gmail.com>
2025-12-02 16:40:23 +0300

    successful revert?

--- UI_BoilerPlate/main.py
+++ UI_BoilerPlate/main.py
@@ -5,54 +5,19 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 
 # Load the kv files
-Builder.load_file("hotbar.kv")
-Builder.load_file("screens.kv")
-
-class Hotbar(FloatLayout):
-    pass
+Builder.load_file("main.kv")
 
 class NavigationManager(ScreenManager):
-    pass
-
-class BoilerPlate(BoxLayout):
     def screen_switcher(self, screen_name):
         # 'nav_manager' will be the id of NavigationManager in KV
         screen_manager = self.ids.nav_manager
         screen_manager.current = screen_name
         print("Screen changed to:", screen_name)
 
-class MainApp(App):
-    def build(self):
-        return BoilerPlate()
-
-if __name__ == "__main__":
-    MainApp().run()
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.screenmanager import ScreenManager
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
-
-# Load the kv files
-Builder.load_file("hotbar.kv")
-Builder.load_file("screens.kv")
-
-class Hotbar(FloatLayout):
-    pass
-
-class NavigationManager(ScreenManager):
-    pass
-
-class BoilerPlate(BoxLayout):
-    def screen_switcher(self, screen_name):
-        # 'nav_manager' will be the id of NavigationManager in KV
-        screen_manager = self.ids.nav_manager
-        screen_manager.current = screen_name
-        print("Screen changed to:", screen_name)
 
 class MainApp(App):
     def build(self):
-        return BoilerPlate()
+        return NavigationManager()
 
 if __name__ == "__main__":
     MainApp().run()
