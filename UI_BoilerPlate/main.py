6a1fe0ce1922be09ce301e94f29bb967f52ef40e
Bones <pentagonzero7@gmail.com>
2025-12-02 16:56:03 +0300

    Vertical Hotbar

--- UI_BoilerPlate/main.py
+++ UI_BoilerPlate/main.py
@@ -1,23 +1,25 @@
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.filechooser import ScreenManager
+from kivy.uix.accordion import NumericProperty
 from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
+import kivy.utils as utils
 
-# Load the kv files
 Builder.load_file("main.kv")
 
-class NavigationManager(ScreenManager):
-    def screen_switcher(self, screen_name):
-        # 'nav_manager' will be the id of NavigationManager in KV
-        screen_manager = self.ids.nav_manager
-        screen_manager.current = screen_name
-        print("Screen changed to:", screen_name)
-
+class NavigationManager(Widget):
+    def screen_switcher(self, instance):
+        ScreenManager = self.ids.current_screen
+        ScreenManager.current = instance
+        print("Screen changed to:", instance)
 
 class MainApp(App):
     def build(self):
         return NavigationManager()
 
+
 if __name__ == "__main__":
     MainApp().run()
+        
