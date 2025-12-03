c08c908bd033ff2a5e920bbdfb6e4518fff527be
Bones <pentagonzero7@gmail.com>
2025-12-03 15:27:37 +0300

    fixed?

--- UI_BoilerPlate/main.py
+++ UI_BoilerPlate/main.py
@@ -6,14 +6,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 import kivy.utils as utils
+from kivy.animation import Animation
 
 Builder.load_file("main.kv")
 
 class NavigationManager(Widget):
-    def screen_switcher(self, instance):
+    def screen_switcher(self, instance, button):
         ScreenManager = self.ids.current_screen
         ScreenManager.current = instance
         print("Screen changed to:", instance)
+
+        animate_button = Animation(
+            background_color=utils.get_color_from_hex("#CCCCCC"), 
+            duration=0.2
+        )
+        animate_button.start(button)
 
 class MainApp(App):
     def build(self):
